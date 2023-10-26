3d1678db123c92564ec19f8abcec1d9e3a3ac892
JotaZonar <jjimenez@zonar.cl>
2023-10-26 07:46:49 -0300

    update

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-VERSION=0.15
+VERSION=0.18
 docker buildx build --push --platform linux/amd64,linux/arm64 -t docker.homejota.net/geoos/zrepo2-plugin:latest -t docker.homejota.net/geoos/zrepo2-plugin:$VERSION .
