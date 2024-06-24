f9ab9367bdb1ad4306082e2c6d33e82fa4881dc9
Jorge Jiménez (Jota) <42505799+JotaZonar@users.noreply.github.com>
2024-06-24 07:32:48 -0400

    ufpr

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-VERSION=0.18
+VERSION=0.19
 docker buildx build --push --platform linux/amd64,linux/arm64 -t docker.homejota.net/geoos/zrepo2-plugin:latest -t docker.homejota.net/geoos/zrepo2-plugin:$VERSION .
